3e97e3839220bc619925931b670480f961dfdaf1
avakanski <avakanski@gmail.com>
2022-09-07 10:53:40 -0600

    Add files via upload

---
+++ Lecture 6 - Exceptions, Modules, Packages/MyMainPackage/MySubPackage/sub_script.py
@@ -0,0 +1,12 @@
+print('I am inside sub_script, which is located in MySubPackage')
+
+X = 23
+print('The value of the variable X is:', X)
+
+def sub_report():
+    Y = 5
+    print('I am a function inside sub_script')
+    print('The value of the variable Y is:', Y)
+
+    
+    
